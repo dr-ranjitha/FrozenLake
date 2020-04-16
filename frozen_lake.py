71f8ce27c3036dedf9b5cb1c54d7335f6426d659
Ranjitha Dhanasekaran <51212846+dr-ranjitha@users.noreply.github.com>
2020-04-16 13:15:42 -0600

    Create frozen_lake.py

---
+++ frozen_lake.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 16 13:14:34 2020
+
+@author: drran
+"""
+import numpy as np
+import gym
+import random
+import time
+from IPython.display import clear_output
+
+env = gym.make("FrozenLake-v0")
