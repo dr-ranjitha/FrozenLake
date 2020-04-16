38964d6f626fc4f2ef00f9c644f19d3b8d848fd0
Ranjitha Dhanasekaran <51212846+dr-ranjitha@users.noreply.github.com>
2020-04-16 15:04:14 -0600

    Update frozen_lake.py
    Q-learning algorithm

--- frozen_lake.py
+++ frozen_lake.py
@@ -28,5 +28,25 @@ discount_rate = 0.99
 exploration_rate = 1
 max_exploration_rate = 1
 min_exploration_rate = 0.01
-exploration_decay_rate = 0.01
+exploration_decay_rate = 0.001
+
+#Hold all rewards from all episodes
+rewards_all_episodes = []
+
+# Q-learning algorithm
+for episode in range(num_episodes):
+    # initialize new episode params
+    state = env.reset()
+    done = False
+    rewards_current_episode = 0
+    
+    for step in range(max_steps_per_episode): 
+        # Exploration-exploitation trade-off
+        # Take new action
+        # Update Q-table
+        # Set new state
+        # Add new reward        
+
+    # Exploration rate decay   
+    # Add current episode reward to total rewards list
 
