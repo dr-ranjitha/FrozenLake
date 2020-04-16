e6ba787f6e0c13a8dcaa407e62c9c8da96e6a242
Ranjitha Dhanasekaran <51212846+dr-ranjitha@users.noreply.github.com>
2020-04-16 14:58:23 -0600

    Update frozen_lake.py
    Initializing Q-Learning Parameters

--- frozen_lake.py
+++ frozen_lake.py
@@ -17,3 +17,16 @@ state_space_size = env.observation_space.n #rows
 action_space_size = env.action_space.n #columns
 
 q_table = np.zeros((state_space_size, action_space_size))
+
+#Initializing Q-Learning Parameters
+num_episodes = 10000
+max_steps_per_episode = 100
+
+learning_rate = 0.1
+discount_rate = 0.99
+
+exploration_rate = 1
+max_exploration_rate = 1
+min_exploration_rate = 0.01
+exploration_decay_rate = 0.01
+
