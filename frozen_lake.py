ba1674fd1e9d0033d9ac6816e177583632641f04
Ranjitha Dhanasekaran <51212846+dr-ranjitha@users.noreply.github.com>
2020-04-16 14:55:17 -0600

    Update frozen_lake.py
    #Create the Q-table

--- frozen_lake.py
+++ frozen_lake.py
@@ -11,3 +11,9 @@ import time
 from IPython.display import clear_output
 
 env = gym.make("FrozenLake-v0")
+
+#Create the Q-table
+state_space_size = env.observation_space.n #rows
+action_space_size = env.action_space.n #columns
+
+q_table = np.zeros((state_space_size, action_space_size))
